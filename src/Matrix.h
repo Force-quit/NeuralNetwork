723aedbcacc08fc900ae84d993a581564bdc36ee
Force-quit <miloulune1@gmail.com>
2024-03-26 10:19:49 -0400

    Updating Matrix again

--- src/Matrix.h
+++ src/Matrix.h
@@ -10,7 +10,7 @@
 
 #include <cassert>
 #include <iostream>
-#include <memory>
+#include <vector>
 
 namespace bpn
 {
@@ -18,19 +18,9 @@ namespace bpn
 	{
 	public:
 		constexpr Matrix(int nRows, int nCols, double value = 0.0) noexcept
-			: nRows{ nRows }, nCols{ nCols }, data(std::make_unique<double[]>(nRows* nCols))
+			: nRows{ nRows }, nCols{ nCols }, data(nRows * nCols, value)
 		{
 			assert(nRows > 0 && nCols > 0 && "Matrix constructor has 0 size");
-			for (int i{}; i < nRows * nCols; ++i)
-			{
-				data[i] = value;
-			}
-		}
-
-		constexpr Matrix(const Matrix& other) noexcept
-			: nRows{ other.nRows }, nCols{ other.nCols }, data(std::make_unique<double[]>(nRows* nCols))
-		{
-			std::copy(data.get(), data.get() + nRows * nCols, other.data.get());
 		}
 
 		// TODO Multidimensional subscript operator when MSVC supports it
@@ -52,6 +42,6 @@ namespace bpn
 	private:
 		const int nRows;
 		const int nCols;
-		const std::unique_ptr<double[]> data;
+		std::vector<double> data;
 	};
 }
